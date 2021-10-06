03c59e33e3f5dcd2b25bfccb946a68f49e7d986f
foxster14 <sarah.fox.h@gmail.com>
2021-10-05 21:15:04 -0500

    added shell commands to provision mysql vm

--- geometry_calculator_sf_Test.py
+++ geometry_calculator_sf_Test.py
@@ -2,21 +2,24 @@
 import unittest
 import geometry_calculator_sf as geo
 from geometry_calculator_sf import userSelection
+import sys 
 from unittest import mock
+from io import StringIO
 import sphere
 
 # PyUnit is an object-oriented framework
 class selectionMenuTest(unittest.TestCase):
     #@mock.patch('geometry_calculator_sf.input', return_value=8)
-    def userInputTest(self):      
+    def testUserInput(self):      
         assert(geo.userSelection(8) == 'Invalid selection, please try again.')
-        #mocked_input.side_effect = ['8']
-        #self.assertTrue(geo.main(),'Invalid selection, please try again.')
+        sys.stdin = StringIO()
+        #m = mocked_input.side_effect = ['8']
+        #self.assertTrue(m,'Invalid selection, please try again.')
         #if input == 8:
             #assert(geo.main() == 'Invalid selection, please try again.')
         #assert(input(8) == 'Invalid selection, please try again.)
     
-    def sphereInputTest(self):
+    def testSphereInput(self):
         assert(sphere.selection() == userSelection(1))
 
 
